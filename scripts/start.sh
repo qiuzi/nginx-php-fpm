60f35f97743dc84672dd2c00e1d2efb25b7c7abb
qiuzi <gxfclql@gmail.com>
2023-03-13 08:38:44 +0000

    up1

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,19 +6,18 @@ if [ "$CONFIG_FILE" != "" ]; then
   echo "[INFO] Config Panel from CONFIG_BASE64 completed"
 fi
 if [ "$DATABASE" = "yes" ]; then
- vendor/bin/phinx migrate && \
- php xcat Tool importAllSettings
+ php xcat Migration new && \
+ php xcat Tool importAllSettings && \
  php xcat Tool createAdmin <<EOF
  $ADMINEMAIL
  $ADMINPASSWD
  Y
 EOF
  php xcat ClientDownload
- bash update.sh
 fi
 # if [ "$PORT" = "" ]; then
 #  PORT=80
 # fi
 # envsubst '$PORT' < /nginx.conf.template > /etc/nginx/nginx.conf
 
-supervisord -c /supervisord.conf
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
